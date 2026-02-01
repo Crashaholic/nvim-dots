e78f2d91071345622fc8376fd9c07b205d58abb4
Crashaholic <33598082+Crashaholic@users.noreply.github.com>
2026-02-02 00:06:48 +0800

    Fixed up telescope getting called before being loaded

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,8 @@
 return {
-	'nvim-telescope/telescope.nvim',
-	tag='v0.2.0',
+	"nvim-telescope/telescope.nvim",
+	version='*',
 	dependencies= {
 		'nvim-lua/plenary.nvim',
 	},
-	config = require('telescope').setup{
-	}
+	opts={}
 }
